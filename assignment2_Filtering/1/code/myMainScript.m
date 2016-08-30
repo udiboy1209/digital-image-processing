e32d7391e751b0e1e8ab9048e5d9bda27aeb24d9
sravps7 <sravps7@gmail.com>
2016-08-30 17:18:45 +0530

    added images and report

--- assignment2_Filtering/1/code/myMainScript.m
+++ assignment2_Filtering/1/code/myMainScript.m
@@ -1,20 +1,22 @@
 %% MyMainScript
 
 tic;
-%% Your code here
+%% myLinearContrastStretching
 img1 = load('../data/superMoonCrop.mat');
 img2 = load('../data/lionCrop.mat');
 
 moon = myLinearContrastStretching(img1.imageOrig);
 lion = myLinearContrastStretching(img2.imageOrig);
 
-sharpened1 = myLinearContrastStretching(myUnsharpMasking(lion,1.6, 1.5));
-sharpened2 = myLinearContrastStretching(myUnsharpMasking(lion, 1.75, 1.5));
-sharpened3 = myLinearContrastStretching(myUnsharpMasking(lion, 1.9, 1.5));
+%% myUnsharpMasking
+sharpened_lion = myLinearContrastStretching(myUnsharpMasking(lion,1.6, 1.5));
+sharpened_moon = myLinearContrastStretching(myUnsharpMasking(moon, 1.8, 1.5));
 
 figure,imshow(lion);
-figure,imshow(sharpened1);
-figure,imshow(sharpened2);
-figure,imshow(sharpened3);
+figure,imshow(sharpened_lion);
+figure,imshow(moon);
+figure,imshow(sharpened_moon);
 
+imwrite(sharpened_lion, '../images/sharpened_lion.png');
+imwrite(sharpened_moon, '../images/sharpened_moon.png');
 toc;
